0dab2237814d77629ad4208edb3684961bb56827
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-07-19 17:37:45 +0530

    first commit

---
+++ Assets/Scripts/EndlessRoad.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class EndlessRoad : MonoBehaviour
+{
+    public int speed = -3;
+
+    private Rigidbody2D rb2d;
+
+    private float roadVerticalLength = 7.5f;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, speed);
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -roadVerticalLength)
+        {
+            RepositionBackground();
+        }
+
+        //TODO
+        //if (EndlessRoad.instance.gameOver == true)
+        //{
+        //   rb2d.velocity = Vector2.zero;
+        //}
+    }
+    private void FixedUpdate()
+    {
+        speed += -1;
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(0, roadVerticalLength * 2f);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private int dir = 0;
+    private Vector3 pos;
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopCoroutine("movePlayer");
+            StartCoroutine("movePlayer");
+            dir = ~dir;
+        }
+    }
+
+    private IEnumerator movePlayer()
+    {
+        if (dir == 0) pos = new Vector3(1.2f, -3f, 0f);
+        else pos = new Vector3(-1.2f, -3f, 0f);
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / 1)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos, t);
+            yield return null;
+        }
+    }
+
+}
